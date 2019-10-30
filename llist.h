570efcca0f86000c3a56771bfbfcaee89aa742eb
rleongPd5 <rleong00@stuy.edu>
2019-10-30 14:43:33 -0400

    Added function headers for Rachel's functions

--- llist.h
+++ llist.h
@@ -10,3 +10,5 @@ void print_list(struct node * n);
 struct node * insert_front(struct node * n, char *newname, char *newartist);
 struct node * free_list(struct node * n);
 struct node * remove_node(struct node *front, char *newname, char *newartist);
+struct node * first_song_by(struct node *n, char *newartist);
+struct node * random_song(struct node *n);
