d3c7aea6981623475635fdbfd0f2a876468f547c
rleongPd5 <rleong00@stuy.edu>
2019-11-01 15:32:22 -0400

    updated the headers

--- llist.h
+++ llist.h
@@ -1,17 +1,19 @@
 #define MAX_NAME_LEN 100
 
-struct node
+struct song_node
 {
   char name[MAX_NAME_LEN];
   char artist[MAX_NAME_LEN];
-  struct node *next;
+  struct song_node *next;
 };
-// struct song_node * table[27];
+// struct song_song_node * table[27];
 
 // from previous assignment
-void print_list(struct node * n);
-struct node * insert_front(struct node * n, char *newname, char *newartist);
-struct node * free_list(struct node * n);
-struct node * remove_node(struct node *front, char *newname, char *newartist);
-struct node * first_song_by(struct node *n, char *newartist);
-struct node * random_song(struct node *n);
+void print_list(struct song_node * n);
+void print_song_node(struct song_node *n);
+struct song_node * insert_front(struct song_node * n, char *newname, char *newartist);
+struct song_node * insert_order(struct song_node *n, char * name, char * artist);
+struct song_node * free_list(struct song_node * n);
+struct song_node * remove_song_node(struct song_node *front, char *newname, char *newartist);
+struct song_node * first_song_by(struct song_node *n, char *newartist);
+struct song_node * random_song(struct song_node *n);
